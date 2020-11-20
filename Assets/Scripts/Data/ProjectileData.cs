9e5a1aecda908e48095911eee8af66b523f16a22
Jim Urbas <git@jimu.net>
2020-11-20 14:19:09 -0600

    Data for Weapons and Projectiles

---
+++ Assets/Scripts/Data/ProjectileData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(fileName = "New Projectile", menuName = "Jamcraft/Projectile")]
+public class ProjectileData : ScriptableObject
+{
+    [Tooltip("Projectile name")]
+    public string projectileName;
+
+    [Tooltip("Prefab used to instantiate this projectile")]
+    public GameObject prefab;
+
+    [Tooltip("Particle Effect on impact")]
+    public ParticleSystem impactVFX;
+
+    [Tooltip("Sound Effect on impact")]
+    public AudioClip impactSFX;
+
+    [Tooltip("Damage this projectile inflicts on target")]
+    public float damage;
+}
---
+++ Assets/Scripts/Data/WeaponData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Recipe
+{
+    public int redBricks;
+    public int blueBricks;
+    public int greenBricks;
+    public int yellowBricks;
+}
+
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Jamcraft/Weapon")]
+public class WeaponData : ScriptableObject
+{
+    [Tooltip("Weapon name")]
+    public string weaponName;
+
+    [Tooltip("Prefab used to instantiate this weapon")]
+    public GameObject prefab;
+
+    [Tooltip("Projectile that this weapon fires")]
+    public ProjectileData projectile;
+
+    [Tooltip("Rounds per second")]
+    public float fireRate;
+
+    [Tooltip("Particle Effect when fired")]
+    public ParticleSystem fireVFX;
+
+    [Tooltip("Sound Effect when fired")]
+    public AudioClip fireSFX;
+
+    [Tooltip("Recipe")]
+    public Recipe recipe;
+
+    //[Tooltip("TBD Cost to upgrade this weapon")]
+    //public int upgradeCost;
+
+    //[Tooltip("TBD Upgrades to this weapon")]
+    //public WeaponData upgrade;
+
+    //[Tooltip("TBD Cost to upgrade this weapon")]
+    //public int upgradeCost;
+}
