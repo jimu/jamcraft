da49ff601cd6f594a67cd18c51e7af9158e09d5a
Jim Urbas <git@jimu.net>
2020-11-23 17:35:39 -0600

    Add pool manager

---
+++ Assets/Scripts/Helpers/Pool.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#pragma warning disable 0649
+
+// implements a pool for a specific prefab
+// used by PoolManager
+public class Pool
+{
+    GameObject prefab;
+    Transform parent;
+
+    public Pool(GameObject prefab, Transform parent)
+    {
+        this.prefab = prefab;
+        this.parent = parent;
+    }
+
+    // Note: msdn documentation claims Lists use an array internally and should perform just as fast
+    List<GameObject> pool = new List<GameObject>();
+    int lastIndex = -1;
+
+    public GameObject Get(Vector3 position)
+    {
+        GameObject o;
+        //Debug.Log($"pool: {pool}");
+        for (int count = pool.Count; count-- > 0;)
+        {
+            lastIndex = (lastIndex + 1) % pool.Count;
+            if (pool[lastIndex] == null)
+                return pool[lastIndex] = GameObject.Instantiate(prefab, position, Quaternion.identity, parent);
+            if (!pool[lastIndex].activeSelf)
+            {
+                o = pool[lastIndex];
+                o.transform.position = position;
+                o.SetActive(true);
+                return o;
+            }
+        }
+        o = GameObject.Instantiate(prefab, position, Quaternion.identity, parent);
+        pool.Add(o);
+        return o;
+    }
+
+    public GameObject Get(Transform newTransform)
+    {
+        GameObject o;
+        //Debug.Log($"pool: {pool}");
+        for (int count = pool.Count; count-- > 0;)
+        {
+            lastIndex = (lastIndex + 1) % pool.Count;
+            if (!pool[lastIndex].activeSelf)
+            {
+                o = pool[lastIndex];
+                o.transform.position = newTransform.position;
+                o.transform.rotation = newTransform.rotation;
+                o.SetActive(true);
+                return o;
+            }
+        }
+        o = GameObject.Instantiate(prefab, newTransform.position, newTransform.rotation, parent);
+        pool.Add(o);
+        return o;
+    }
+
+}
---
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Maintains a list of Pools keyed on prefabs
+ */
+
+public class PoolManager : MonoSingleton<PoolManager>
+{
+    public Dictionary<GameObject, Pool> Pools = new Dictionary<GameObject, Pool>();
+
+    public Pool GetPool(GameObject prefab)
+    {
+        return Pools.ContainsKey(prefab) ? 
+            Pools[prefab] :
+            Pools[prefab] = new Pool(prefab, transform);
+    }
+
+    public GameObject Get(GameObject prefab, Vector3 position)
+    {
+        return GetPool(prefab).Get(position);
+    }
+    public GameObject Get(GameObject prefab, Transform parent)
+    {
+        return GetPool(prefab).Get(parent);
+    }
+}
