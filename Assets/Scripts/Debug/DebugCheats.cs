8f3fdea7dc3b5b8a5fa06fba61602a2f5279f0a8
Jim Urbas <git@jimu.net>
2020-11-28 06:36:06 -0600

    Implement Win/Lose screen and cheat keys
    Added delay to ending scene to allow brick explosions to complete

---
+++ Assets/Scripts/Debug/DebugCheats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCheats : MonoBehaviour
+{
+   // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+            GameManager.Instance.Lose();
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+            GameManager.Instance.Win();
+        if (Input.GetKeyDown(KeyCode.Backslash))
+            DumpStats();
+    }
+
+    void DumpStats()
+    {
+        Debug.Log($"Time Scale: {Time.timeScale}");
+    }
+}
+
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-#pragma warning disable 0649
+
+enum GameState { Error, Start, Play, Pause, Help, Win, Lose };
+
+#pragma warning disable 0649,414
 public class GameManager : MonoSingleton<GameManager> {
 
     [Header("Camera")]
@@ -13,37 +17,67 @@ public class GameManager : MonoSingleton<GameManager> {
     public CPUController enemy;
 
     [Header("UI")]
-    [SerializeField] private GameObject winText;
-    [SerializeField] private GameObject loseText;
+    [SerializeField] private GameObject winText; // TODO delete
+    [SerializeField] private GameObject loseText; // TODO delete
+    [SerializeField] float loseDelay = 2f;
+    [SerializeField] float winDelay = 4f;
 
-    private bool isGameOver = false;
-
-    // Start is called before the first frame update
-    void Start() {
+    private GameState state = GameState.Error;
 
 
+    private void Start()
+    {
+        SetGameState(GameState.Play);
     }
+
 
     // Update is called once per frame
     void Update() {
 
-        if(!isGameOver) {
-            if(player.homeBase.isDead) {
-                loseText.SetActive(true);
-                GameOver();
-            }
+        if(state == GameState.Play) {
 
-            if(enemy.homeBase.isDead) {
-                winText.SetActive(true);
-                GameOver();
-            }
+            if (enemy.homeBase.isDead)
+                Win();
+
+            if (player.homeBase.isDead)
+                Lose();
         }
 
     }
 
-    private void GameOver() {
-        isGameOver = true;
-        Time.timeScale = 0;
+    void SetGameState(GameState state)
+    {
+        this.state = state;
+        Time.timeScale = (state == GameState.Play || state == GameState.Lose || state == GameState.Win) ? 1 : 0;
     }
 
+
+    public void Win()
+    {
+        Debug.Log("Win()");
+        SetGameState(GameState.Win);
+
+        // Wait for explosions / FX
+        // TODO - calculate score and HighScores scene instead?   Preserve score and load next scene?
+        StartCoroutine(DelayedSceneLoad("Menu Win", winDelay));
+    }
+
+    public void Lose()
+    {
+        Debug.Log("Lose()");
+
+        SetGameState(GameState.Lose);
+
+        // Wait for explosions / FX
+        // TODO - calculate cumulative score from all levels and load HighScores scene instead?
+        StartCoroutine(DelayedSceneLoad("Menu Lose", winDelay));
+
+    }
+
+
+    public IEnumerator DelayedSceneLoad(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
 }
