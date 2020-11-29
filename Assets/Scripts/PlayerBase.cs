7a376f0fef466fa7969d732815531e7f19b708cc
Scarmez <mscarmozzino@gmail.com>
2020-11-29 23:12:29 +1100

    Enemy bots will now drop a random brick specified in GameManager possibleLoot
    Also fixed bots stopping after an encounter

--- Assets/Scripts/PlayerBase.cs
+++ Assets/Scripts/PlayerBase.cs
@@ -16,6 +16,14 @@ public class PlayerBase : MonoBehaviour {
             if(bots[i].isDead) {
                 Debug.Log(bots[i].transform.name + " Died");
                 GameObject bot = bots[i].gameObject;
+
+                // GIVE PLAYER RESOURCE
+                if(bots[i].alignment == BotController.Alignment.CPU) {
+                    if(GameManager.Instance.possibleLoot.Length > 0) {
+                        Crafter.Instance.AddToInventory(GameManager.Instance.possibleLoot[Random.Range(0, GameManager.Instance.possibleLoot.Length-1)]);
+                    }
+                }
+
                 bots.RemoveAt(i);
                 //Destroy(bot);   explode it instead and then wait 10 seconds before destroying the exploded bot
             }
