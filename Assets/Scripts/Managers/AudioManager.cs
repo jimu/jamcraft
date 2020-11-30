cec1bcc8818fc81aabddad11fd2baaead209049d
Jim Urbas <git@jimu.net>
2020-11-30 03:51:27 -0600

    Add AudioManager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoSingleton<AudioManager>
+{
+    AudioSource audioSource;
+    
+    protected override void Init()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOneShot(AudioClip sfx)
+    {
+        audioSource.PlayOneShot(sfx);
+    }
+}
